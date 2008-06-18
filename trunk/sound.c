d6a6ff5cd101277c2914014163df4574dd15f45a
dash <dash@c2453f25-0b60-48b7-907d-5cf9e6e26ee2>
2008-06-18 15:36:31 +0000

    Adding some sound functionality. No basic commands to invoke it yet...

---
+++ trunk/sound.c
@@ -0,0 +1,82 @@
+#include "misc.h"
+#include <math.h>
+
+#define SAMPLING_RATE 48000
+#define FRAGSIZE 2048
+#define PIECES 16
+
+#define FFIX (4294967296.0/SAMPLING_RATE)
+
+void fillaudio(void *data, Uint8 *buffer, int len)
+{
+bc *bc=data;
+int i,j, sp;
+Sint16 *p;
+sound *s;
+int accum[FRAGSIZE*2/PIECES], *ap;
+Uint32 v, dv;
+
+	p = (void *)buffer;
+	len>>=2; // stereo, 16 bit signed samples = 4 bytes/sample
+
+	len /= PIECES;
+	for(j=0;j<PIECES;++j)
+	{
+		memset(accum, 0, sizeof(accum));
+		for(sp = 0;sp < MAX_SOUNDS;++sp)
+		{
+			s=bc->sounds + sp;
+			if(~s->flags & SND_ACTIVE)
+				continue;
+			ap = accum;
+			v = s->index;
+			dv = FFIX * s->frequency;
+			for(i=0;i<len;++i)
+			{
+				v += dv;
+				ap[i*2+0] += ((v>>19) & 0x1fff) - 4096;
+				ap[i*2+1] += ((v>>19) & 0x1fff) - 4096;
+			}
+			s->index = v;
+//			s->frequency *= 1.0001;
+		}
+		ap = accum;
+		for(i=0;i<len;++i)
+		{
+			*p++ = *ap++;
+			*p++ = *ap++;
+		}
+	}
+}
+
+void soundopen(bc *bc)
+{
+SDL_AudioSpec wanted;
+
+	memset(&wanted,0,sizeof(wanted));
+	wanted.freq=SAMPLING_RATE;
+	wanted.channels=2;
+	wanted.format=AUDIO_S16;
+	wanted.samples=FRAGSIZE;
+	wanted.callback=fillaudio;
+	wanted.userdata=bc;
+
+	if(SDL_OpenAudio(&wanted,0)<0)
+	{
+		fprintf(stderr,"Couldn't open audio: %s\n",SDL_GetError());
+		return;
+	}
+	bc->soundworking = 1;
+#if 0
+	bc->sounds[0].flags = SND_ACTIVE;
+	bc->sounds[0].frequency = 110.0;// * pow(2.0, 5.0/12.0);;
+	bc->sounds[1].flags = SND_ACTIVE;
+	bc->sounds[1].frequency = bc->sounds[0].frequency * pow(2.0, 4.0/12.0);
+	bc->sounds[2].flags = SND_ACTIVE;
+	bc->sounds[2].frequency = bc->sounds[0].frequency * pow(2.0, 7.0/12.0);
+	bc->sounds[3].flags = SND_ACTIVE;
+	bc->sounds[3].frequency = bc->sounds[0].frequency * pow(2.0, 12.0/12.0);
+#endif
+
+	SDL_PauseAudio(0);
+}
